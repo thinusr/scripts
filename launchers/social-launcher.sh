3380e69e8f3e1ceb189fd0a157c89e90540e6172
thinusr <thinusr@gmail.com>
2025-10-13 10:22:59 +0200

    added webapp launchers scripts

---
+++ launchers/social-launcher.sh
@@ -0,0 +1,38 @@
+APP_DIR="$HOME/.local/share/applications/webapps/social"
+
+declare -A app_map
+for file in "$APP_DIR"/*.desktop; do
+    [ -f "$file" ] || continue
+    name=$(grep -i '^Name=' "$file" | cut -d'=' -f2)
+    [ -n "$name" ] && app_map["$name"]="$file"
+done
+
+options=("${!app_map[@]}")
+
+choice=$(printf "%s\n" "${options[@]}" | sort | rofi -dmenu -p "Social Webapps" -theme-str '
+window {
+    location: center;
+    anchor: center;
+    width: 250px;
+}
+listview {
+    lines: 5;
+    fixed-height: true;
+}
+element-text {
+    font: "JetBrainsMono Nerd Font 16";
+}')
+
+[ -z "$choice" ] && exit 0
+
+desktop_file="${app_map[$choice]}"
+[ -z "$desktop_file" ] && notify-send "Social Launcher" "Could not find desktop file for $choice" && exit 1
+
+# Get exact Exec line (keep all flags)
+cmd=$(grep -i '^Exec=' "$desktop_file" | head -n1 | cut -d'=' -f2-)
+
+# Launch
+bash -c "$cmd" &
+
+notify-send "Social Launcher" "Launching $choice…"
+
